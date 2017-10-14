c149e3bbb426bb924447bbc20e1f82bc21519f98
resurtm <resurtm@gmail.com>
2017-10-14 20:49:56 +0600

    wip

--- tests/test_cube.py
+++ tests/test_cube.py
@@ -5,8 +5,18 @@ from tests.base import TestCase
 
 class TestCube(TestCase):
     def test_common(self):
-        self.assertTrue(self.f.name == 'cube.obj')
+        self.assertEqual(self.f.name, 'cube.obj')
         self.assertTrue(len(self.f.objects) == 1)
+
+    def test_materials(self):
+        libs = self.f.material_libs
+        self.assertTrue(len(libs) == 1)
+        self.assertEqual(libs['cube.mtl'].name, 'cube.mtl')
+        self.assertTrue(len(libs['cube.mtl'].materials) == 1)
+
+        mat = libs['cube.mtl'].materials['cube']
+        self.assertEqual(mat.name, 'cube')
+        self.assertEqual(mat.material_lib, libs['cube.mtl'])
 
     @staticmethod
     def object_name():
